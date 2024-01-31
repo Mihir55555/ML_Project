2f4ec05a5e553d8c459851ee42fbf78718301d71
Mihir sarvan <mihirsarvan193@gmail.com>
2024-01-31 18:05:34 +0530

    small change

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 import sys 
 
 def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb = error-detail.exc_info()
+    _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message="error occured in python script name [{0}] line number [{1}] error_message [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
     
