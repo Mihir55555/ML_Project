47a32e4f19671ba1b18447f645c5b4bf047b5cbb
Mihir sarvan <mihirsarvan193@gmail.com>
2024-02-03 21:43:21 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
-import sys 
+import sys
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -14,4 +15,4 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
-    
+ 
