e62a3ef247e32a8a31391fec58ed2b43e83930c4
Mihir sarvan <mihirsarvan193@gmail.com>
2024-02-04 12:42:39 +0530

    Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,3 +1,5 @@
+# read data from source and then train,test & split data and save data
+
 import os
 import sys
 from src.exception import CustomException
@@ -6,6 +8,9 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionconfig:
@@ -50,4 +55,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
+    data_tranformation = DataTransformation()
+    data_tranformation.initiate_data_transformation(train_data,test_data)
